614d1006967c3d9a09d780879d39c76110de4bc6
fangzongzhou <fangzongzhou@thunisoft.com>
2017-05-23 21:14:31 +0800

    瞎改

--- src/main/java/com/fzz/service/DocService.java
+++ src/main/java/com/fzz/service/DocService.java
@@ -1,7 +1,7 @@
 package com.fzz.service;
-
-import org.apache.poi.hwpf.HWPFDocument;
-import org.apache.poi.hwpf.usermodel.Range;
+//
+//import org.apache.poi.hwpf.HWPFDocument;
+//import org.apache.poi.hwpf.usermodel.Range;
 
 import java.io.*;
 import java.util.HashMap;
@@ -13,22 +13,22 @@ import java.util.Map;
 public class DocService {
 
     private void build(File tmpFile, Map<String, String> contentMap, String exportFile) throws Exception {
-        FileInputStream tempFileInputStream = new FileInputStream(tmpFile);
-
-        HWPFDocument document = new HWPFDocument(tempFileInputStream);
-        // 读取文本内容
-        Range bodyRange = document.getRange();
-        // 替换内容
-        for (Map.Entry<String, String> entry : contentMap.entrySet()) {
-            bodyRange.replaceText("${" + entry.getKey() + "}", entry.getValue());
-        }
-
-        //导出到文件
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        document.write(byteArrayOutputStream);
-        OutputStream outputStream = new FileOutputStream(exportFile);
-        outputStream.write(byteArrayOutputStream.toByteArray());
-        outputStream.close();
+//        FileInputStream tempFileInputStream = new FileInputStream(tmpFile);
+//
+//        HWPFDocument document = new HWPFDocument(tempFileInputStream);
+//        // 读取文本内容
+//        Range bodyRange = document.getRange();
+//        // 替换内容
+//        for (Map.Entry<String, String> entry : contentMap.entrySet()) {
+//            bodyRange.replaceText("${" + entry.getKey() + "}", entry.getValue());
+//        }
+//
+//        //导出到文件
+//        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+//        document.write(byteArrayOutputStream);
+//        OutputStream outputStream = new FileOutputStream(exportFile);
+//        outputStream.write(byteArrayOutputStream.toByteArray());
+//        outputStream.close();
     }
 
 
